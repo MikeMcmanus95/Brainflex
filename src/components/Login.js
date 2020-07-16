907b41d796000b518e87cab7f805037aca0bcc7b
Michael Mcmanus <mikemcmanus95@gmail.com>
2020-07-15 23:54:18 -0400

    add: login form

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,34 @@
 import React from 'react';
+import useInput from '../hooks/useInput';
 
 const Login = () => {
-  return <div></div>;
+  const { value: email, bind: bindEmail, reset: resetEmail } = useInput('');
+  const { value: pass, bind: bindPass, reset: resetPass } = useInput('');
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    console.log(email, pass);
+    resetEmail();
+    resetPass();
+  };
+
+  return (
+    <div>
+      <div>
+        <h1>Log In</h1>
+        <form onSubmit={handleSubmit}>
+          <input type="text" placeholder="Email" {...bindEmail} required />
+          <input
+            type="password"
+            placeholder="Password"
+            {...bindPass}
+            required
+          />
+          <button type="submit">Login</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
